4ccee102eacf1ad99ac32a53c6436f40d48a46cb
deweieiei <babydew2015@gmail.com>
2022-08-07 13:51:28 +0700

    Up

--- projectchang02/windows/flutter/generated_plugin_registrant.h
+++ projectchang02/windows/flutter/generated_plugin_registrant.h
@@ -7,8 +7,7 @@
 #ifndef GENERATED_PLUGIN_REGISTRANT_
 #define GENERATED_PLUGIN_REGISTRANT_
 
-// #include <flutter/plugin_registry.h>
-#include<flutter/plugin_registry.h>
+#include <flutter/plugin_registry.h>
 
 // Registers Flutter plugins.
 void RegisterPlugins(flutter::PluginRegistry* registry);
